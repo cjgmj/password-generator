306331289924133fad12732359027701793a235c
cjgmj <cjgmj@hotmail.com>
2021-11-19 16:27:52 +0100

    Remove dist

--- dist/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const terminal_password_generator_1 = require("./infrastructure/terminal-password-generator");
-const terminalPasswordGenerator = new terminal_password_generator_1.TerminalPasswordGenerator();
-console.log(terminalPasswordGenerator.createPassword().getValue());
