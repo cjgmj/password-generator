a13edc5e0b524c6aadb04d27473924cf082d04d1
cjgmj <cjgmj@hotmail.com>
2021-11-19 17:20:46 +0100

    Tests

--- test/password-generator-test.ts
+++ test/password-generator-test.ts
@@ -4,61 +4,67 @@ import {PasswordOptions} from "../src/application/password-options";
 import {Password} from "../src/domain/password/password";
 
 describe("Password generator", () => {
-    it("should return a password with default options" ,() => {
+    it("should return a password with default options", () => {
         const passwordGenerator: PasswordGenerator = new PasswordGenerator();
         const options: PasswordOptions = new PasswordOptions();
 
         const password: Password = passwordGenerator.execute(options);
 
         expect(password.getValue()).to.match(/[a-zA-Z0-9~!@#$%^&*+\-\/.,{}[\]();:|?<>="`]/g);
+        expect(password.getValue().length).to.equal(16);
     });
 
-    it("should return a password without symbols" ,() => {
+    it("should return a password without symbols", () => {
         const passwordGenerator: PasswordGenerator = new PasswordGenerator();
         const options: PasswordOptions = new PasswordOptions(true, true, true, false);
 
         const password: Password = passwordGenerator.execute(options);
 
         expect(password.getValue()).to.match(/[a-zA-Z0-9]/g);
+        expect(password.getValue().length).to.equal(16);
     });
 
-    it("should return a password without numeric" ,() => {
+    it("should return a password without numeric", () => {
         const passwordGenerator: PasswordGenerator = new PasswordGenerator();
         const options: PasswordOptions = new PasswordOptions(true, true, false, true);
 
         const password: Password = passwordGenerator.execute(options);
 
         expect(password.getValue()).to.match(/[a-zA-Z~!@#$%^&*+\-\/.,{}[\]();:|?<>="]/g);
+        expect(password.getValue().length).to.equal(16);
     });
 
-    it("should return a password without upper case" ,() => {
+    it("should return a password without upper case", () => {
         const passwordGenerator: PasswordGenerator = new PasswordGenerator();
-        const options: PasswordOptions = new PasswordOptions(true, true, false, true);
+        const options: PasswordOptions = new PasswordOptions(true, false, true, true);
 
         const password: Password = passwordGenerator.execute(options);
 
         expect(password.getValue()).to.match(/[a-z0-9~!@#$%^&*+\-\/.,{}[\]();:|?<>="]/g);
+        expect(password.getValue().length).to.equal(16);
     });
 
-    it("should return a password without lower case" ,() => {
+    it("should return a password without lower case", () => {
         const passwordGenerator: PasswordGenerator = new PasswordGenerator();
-        const options: PasswordOptions = new PasswordOptions(true, true, false, true);
+        const options: PasswordOptions = new PasswordOptions(false, true, true, true);
 
         const password: Password = passwordGenerator.execute(options);
 
         expect(password.getValue()).to.match(/[A-Z0-9~!@#$%^&*+\-\/.,{}[\]();:|?<>="]/g);
+        expect(password.getValue().length).to.equal(16);
     });
 
-    it("should return a password with custom symbols" ,() => {
+    it("should return a password with custom symbols", () => {
         const passwordGenerator: PasswordGenerator = new PasswordGenerator();
         const options: PasswordOptions = new PasswordOptions(true, true, true, true, "~!@#$%");
 
         const password: Password = passwordGenerator.execute(options);
 
         expect(password.getValue()).to.match(/[A-Z0-9~!@#$%"]/g);
+        expect(password.getValue().length).to.equal(16);
     });
 
-    it("should return a password with length four" ,() => {
+    it("should return a password with length four", () => {
         const passwordGenerator: PasswordGenerator = new PasswordGenerator();
         const options: PasswordOptions = new PasswordOptions(true, true, true, true, "~!@#$%", 4);
 
@@ -67,4 +73,14 @@ describe("Password generator", () => {
         expect(password.getValue()).to.match(/[A-Z0-9~!@#$%"]/g);
         expect(password.getValue().length).to.equal(4);
     });
+
+    it("should return a password with length four as the option length is less than four", () => {
+        const passwordGenerator: PasswordGenerator = new PasswordGenerator();
+        const options: PasswordOptions = new PasswordOptions(true, true, true, true, "~!@#$%", 3);
+
+        const password: Password = passwordGenerator.execute(options);
+
+        expect(password.getValue()).to.match(/[A-Z0-9~!@#$%"]/g);
+        expect(password.getValue().length).to.equal(4);
+    })
 });
